2e316e25d116304904270e86188c86bd192231cc
JhowQT <jhon.q.torrez@gmail.com>
2025-03-06 18:39:03 -0300

    Criação do evento com tag form a partir do submit e criação do FOREACH para exibir na tela criando um li onde ele recebera os valores do objeto

--- js/index.js
+++ js/index.js
@@ -3,5 +3,35 @@ const resp = document.querySelector("ul")                                       
 
 
 let tarefas = [
-    ...[{ idTarefa: "1", tituloTarefa: "Tarefa Inicial", concluido: false }]                // cria um array de um unico objeto e acrescenta ao array TEREFAS com o metodo SPREAD
+    ...[{ idTarefa: "teste3", tituloTarefa: "testando obj", concluido: false }]                // cria um array de um unico objeto e acrescenta ao array TEREFAS com o metodo SPREAD
 ]
+
+
+
+frm.addEventListener("submit", (e) => {                                                     // Criação do Evento para capturar os valores
+    e.preventDefault()
+
+
+    const idTarefa = frm.inId.value                                                         // Pega o Valor do ID
+    const tituloTarefa = frm.inTitulo.value                                                 // Pega o Valot do TItulo
+    const concluido = false                                                                // Valor fixo da VARIAVEL concluido para FALSE 
+
+    tarefas = [...tarefas, { idTarefa, tituloTarefa, concluido }]                           // usa SPREAD para reatribuir os objetos novos a lista junto com os valores anteriores da lista    
+
+    resp.innerHTML = ""                                                                     // Limpa a TAG ul cada vez que o evento for acionado
+    
+    tarefas.forEach((tarefa, indiceLista) => {                                              // Cria um forEACh para iterar nos indices e elementos e seus valores 
+
+        const li = document.createElement("li")                                             // variavel que cria a tag li dentro da TAG ul
+
+        li.innerHTML = `                                                                    
+        ID da Tarefa: ${tarefa.idTarefa} -  
+        Título da Tarefa: ${tarefa.tituloTarefa} - 
+        Concluido: ${tarefa.concluido ? "sim":"não"}`                                       // A variavel LI recebe os valores de cada elemento e seus valores dos objetos
+
+        resp.appendChild(li)                                                                // A tag resp ira incomporar os valores do FOREACH
+    })
+   
+    frm.reset()                                                                             // Limpa todos os campos do form
+    frm.inId.focus()                                                                        // Foca na tag INPUT com valor InID
+});
